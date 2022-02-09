950b370947b308b6d16080da80599716a8021ac0
Mark Weston <info@markweston.co.uk>
2022-02-09 23:41:51 +0000

    Add custom entry template for cards Layout cards in a grid using flex Add transparent overlay to card images

--- archive-sector.php
+++ archive-sector.php
@@ -15,22 +15,29 @@ wp_rig()->print_styles( 'wp-rig-content' );
 
 ?>
 	<main id="primary" class="site-main">
-		<?php
-		if ( have_posts() ) {
+		<div class="inner">
+			<?php
+			if ( have_posts() ) {
 
-			get_template_part( 'template-parts/content/page_header' );
+				get_template_part( 'template-parts/content/page_header' );
 
-			while ( have_posts() ) {
-				the_post();
+				// post_type_archive_title();
 
-				get_template_part( 'template-parts/content/entry', get_post_type() );
+				echo '<div class="card-wrapper">';
+				echo '<div class="card archive-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>';
+				while ( have_posts() ) {
+					the_post();
+
+					get_template_part( 'template-parts/content/entry-card', get_post_type() );
+				}
+				echo '</div>';
+
+				get_template_part( 'template-parts/content/pagination' );
+			} else {
+				get_template_part( 'template-parts/content/error' );
 			}
-
-			get_template_part( 'template-parts/content/pagination' );
-		} else {
-			get_template_part( 'template-parts/content/error' );
-		}
-		?>
+			?>
+		</div>
 	</main><!-- #primary -->
 <?php
 get_sidebar();
