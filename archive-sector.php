c54a5dc9ca840207e212d30d2ddf470164df86cb
Mark Weston <info@markweston.co.uk>
2022-02-22 22:07:15 +0000

    Add our services content

--- archive-sector.php
+++ archive-sector.php
@@ -38,6 +38,41 @@ wp_rig()->print_styles( 'wp-rig-content' );
 			?>
 		</div>
 		<?php get_template_part( 'template-parts/content/promo-block' ); ?>
+		<div class="panel">
+    		<div class="inner">
+        		<div class="row">
+            		<div class="col col-dir-col col--center reset-margin-top reset-margin-bottom">
+						<h2 class="title__main"><span>Our</span> Services</h2>
+						<div class="services">
+							<div class="service service--build">
+								<h3 class="service__title">BUILD CONSULTANCY</h3>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+							</div>
+							<div class="service service--health">
+								<h3 class="service__title">HEALTH & SAFETY</h3>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+							</div>
+							<div class="service service--sustainable">
+								<h3 class="service__title">SUSTAINABILITY PRACTICES</h3>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+							</div>
+							<div class="service service--budget">
+								<h3 class="service__title">BUDGET PLANNING</h3>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+							</div>
+							<div class="service service--project">
+								<h3 class="service__title">PROJECT MANAGEMENT</h3>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+							</div>
+							<div class="service service--community">
+								<h3 class="service__title">COMMUNITY BUILDING</h3>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	</main><!-- #primary -->
 <?php
 get_footer();
